c1c754081b21833b83dc00d054c3e048d65774d1
Christopher Chong <t.kleinchong@gmail.com>
2023-06-27 01:09:39 +0800

    Correct typo.

--- src/domain/cli.rs
+++ src/domain/cli.rs
@@ -1,8 +1,8 @@
 use structopt::StructOpt;
 
-/// Fields loaded from the command line when launching a service.
+/// Fields loaded from the command line when launching this server.
 #[derive(Clone, Debug, StructOpt)]
-#[structopt(name = "Flight Itinerary Microservice")]
+#[structopt(name = "Notary Server")]
 pub struct CliFields {
     /// Configuration file location
     #[structopt(long, default_value = "./src/config/config.yaml")]
