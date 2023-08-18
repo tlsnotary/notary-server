9a83d96cf32750350902958574e96302f0e8211d
Christopher Chong <t.kleinchong@gmail.com>
2023-08-18 16:58:26 +0800

    Fix failing tests due to tlsn server refactoring. (#7)
    * Add more README.
    
    * Test changing comment.
    
    * Test changing unwrap pattern.
    
    * Fix failing test.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -14,7 +14,7 @@ use std::{
     sync::Arc,
     time::Duration,
 };
-use tls_server_fixture::{bind_test_server, CA_CERT_DER, SERVER_DOMAIN};
+use tls_server_fixture::{bind_test_server_hyper, CA_CERT_DER, SERVER_DOMAIN};
 use tlsn_prover::{bind_prover, ProverConfig};
 use tokio_rustls::TlsConnector;
 use tokio_util::compat::{FuturesAsyncReadCompatExt, TokioAsyncReadCompatExt};
@@ -180,7 +180,7 @@ async fn test_tcp_prover() {
 
     // Connect to the Server
     let (client_socket, server_socket) = tokio::io::duplex(2 << 16);
-    let server_task = tokio::spawn(bind_test_server(server_socket.compat()));
+    let server_task = tokio::spawn(bind_test_server_hyper(server_socket.compat()));
 
     let mut root_store = tls_core::anchors::RootCertStore::empty();
     root_store
@@ -349,7 +349,7 @@ async fn test_websocket_prover() {
 
     // Connect to the Server
     let (client_socket, server_socket) = tokio::io::duplex(2 << 16);
-    let server_task = tokio::spawn(bind_test_server(server_socket.compat()));
+    let server_task = tokio::spawn(bind_test_server_hyper(server_socket.compat()));
 
     let mut root_store = tls_core::anchors::RootCertStore::empty();
     root_store
